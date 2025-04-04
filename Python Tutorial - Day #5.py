2898d0d663a4c0b2758b59362a5ab64fbf3527a2
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-04 21:17:37 +0545

    Update Python Tutorial - Day #5.py

--- Python Tutorial - Day #5.py
+++ Python Tutorial - Day #5.py
@@ -2,4 +2,11 @@
 # It is written by Amrit
 #Enter ctrl+/ to comment in/out the comment
 
-print("Are you a good boy ? \n\n I am also a good boy") #Good boy
+'''
+Will this be printed ?
+No cuz it's commented
+'''
+
+print("Are you a \"good boy?\" \n\n I am also a good boy") #Good boy
+
+print("Are you", 17, 18, "yo?",sep=" or " )
