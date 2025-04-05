b77c8b2fb42434ecc6c1086cbce34dc80d326133
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-05 21:15:54 +0545

    Update Python Tutorial - Day #6.py

--- Python Tutorial - Day #6.py
+++ Python Tutorial - Day #6.py
@@ -1,1 +1,23 @@
+a = 100999
+b = "Amrit"
+print(a)
+print(b)
 
+c= 2 #Int
+print(type(c))
+
+d= 2.1 #Float
+print(type(d))
+
+e= complex(8,2) #Complex
+print(type(e))
+print(e)
+
+ab = [a,b] #list
+print(ab)
+
+ab1 = ("Arjun","Shanti ") #tuple
+print(ab1) 
+
+dict = {"Name":"Amrit","Bro":"Ankit","Age":69,"Able to vote ?":"No"} #dictionary
+print(dict)
