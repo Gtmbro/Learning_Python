4bfa1be413f7f10439995a9b5efc8779a0c1ca10
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-10 21:02:23 +0545

    Update Python Tutorial - Day #10 ( User Input ) .py

--- Python Tutorial - Day #10 ( User Input ) .py
+++ Python Tutorial - Day #10 ( User Input ) .py
@@ -1,1 +1,8 @@
+a = input("What is your name ? ")
+print("Oh hey!", a )
 
+
+x = input("Enter anything: ")
+y = input("Enter anything else: ")
+print(x+y) # This will concatenate the two inputs (No matter string or number )
+print(int(x)+int(y)) # This will add the two inputs(if they are numbers)
