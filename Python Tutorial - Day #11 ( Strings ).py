7633127cb352958f1839260a75dd084cbdd3e118
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-11 22:30:53 +0545

    Update Python Tutorial - Day #11 ( Strings ).py

--- Python Tutorial - Day #11 ( Strings ).py
+++ Python Tutorial - Day #11 ( Strings ).py
@@ -1,1 +1,24 @@
+a1 = 'Amrit'
+print('A girl said,"Do you love me,', a1+'?"')
+print()
 
+boy1 = '''Boy 1:-
+Hey bro..
+What you doing tonight ?
+Wanna hangout sometimes??
+\n'''
+boy2 = '''Boy 2:-
+Nah bro..
+I can't, 
+I am really busy rn.
+'''
+
+print(boy1+boy2)
+
+print(a1[0])
+print(a1[-3]) #Len a1-3
+#print(a1[5]) #Error
+print()
+
+for i in boy1:
+  print(i)
