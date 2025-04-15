67116013b9b179bfb6aa428f8cd84e105ba157bd
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-15 20:47:05 +0545

    Update Python Tutorial - Day #14 ( If Else Conditional Statements ).py

--- Python Tutorial - Day #14 ( If Else Conditional Statements ).py
+++ Python Tutorial - Day #14 ( If Else Conditional Statements ).py
@@ -1,1 +1,25 @@
+a = int(input("Hey bro ! Can you please enter your age ? "))
+
+# Conditional operators are: > , < , >= , <= , == , != 
+
+if a>=18:
+  print()
+  print("Yo! My man is eligible to vote.. ")
+elif a<18:
+  print()
+  print("Don't worry man ! You will soon be able to vote.. ")
+print()
+print("Checking eligibility for voting done! ")
+
+if a==0:
+  print()
+  print("You are neither even nor odd years old.. ")
+elif a%2==0:
+  print()
+  print("You are even years old.. ")
+else:
+  print()
+  print("You are odd years old.. ")
+print()
+print("Checking even odd done! ")
 
