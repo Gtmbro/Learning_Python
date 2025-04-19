46a073b5ec20d1f64390063aaf736094826e79fb
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-19 22:29:53 +0545

    Update Python Tutorial - Day #15 ( Good Morning Sir ).py

--- Python Tutorial - Day #15 ( Good Morning Sir ).py
+++ Python Tutorial - Day #15 ( Good Morning Sir ).py
@@ -1,1 +1,22 @@
+import time
+timestamp1 = time.strftime("%Y:%m:%d:%A") #%Y=YEAR, %m=MONTH, %d=day, %A= day of the week
+timestamp2 = time.strftime("%H:%M:%S") #%H=HOUR, %M=MINUTE, %S=SECOND
 
+print(timestamp1) # Format: YYYY:MM:DD
+print(timestamp2) # Format: HH:MM:SS
+print()
+
+if "5:00:00">=timestamp2<"12:00:00":
+  print("Good Morning brotha! ")
+  
+elif "12:00:00">=timestamp2<"17:00:00":
+  print("Good Afternoon brotha! ")
+  
+elif "17:00:00">=timestamp2<"21:00:00":
+  print("Good Evening brotha! ")
+  
+elif "21:00:00">=timestamp2<"5:00:00":
+  print("Good Night brotha! ")
+  
+
+  
