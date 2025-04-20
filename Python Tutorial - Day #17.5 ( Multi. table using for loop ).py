6d5f39ebb9e328d2dd2d99522b3a5af3cbd01a65
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-20 22:16:01 +0545

    Create Python Tutorial - Day #17.5 ( Multi. table using for loop ).py

---
+++ Python Tutorial - Day #17.5 ( Multi. table using for loop ).py
@@ -0,0 +1,70 @@
+#Multiplication table from 1 to 10
+
+print("Multiplication table of 1:-\n")
+for i in range(1,11): #range(starting no., one no. after ending no.)
+  i = 1*i
+  print("1 x",i,"=",i)
+print()
+
+print("Multiplication table of 2:-\n")
+for j in range(1,11): #range(starting no., one step after ending no.)
+  j1 = j
+  j = 2*j
+  print("2 x",j1,"=",j)
+print()
+
+print("Multiplication table of 3:-\n")
+for k in range(1,11): #range(starting no., one step after ending no.)
+  k1 = k
+  k = 3*k
+  print("3 x",k1,"=",k)
+print()
+
+print("Multiplication table of 4:-\n")
+for L in range(1,11): #range(starting no., one step after ending no.)
+  L1 = L
+  L = 4*L
+  print("4 x",L1,"=",L)
+print()
+
+print("Multiplication table of 5:-\n")
+for m in range(1,11): #range(starting no., one step after ending no.)
+  m1 = m
+  m = 5*m
+  print("5 x",m1,"=",m)
+print()
+
+print("Multiplication table of 6:-\n")
+for n in range(1,11): #range(starting no., one step after ending no.)
+  n1 = n
+  n = 6*n
+  print("6 x",n1,"=",n)
+print()
+
+print("Multiplication table of 7:-\n")
+for o in range(1,11): #range(starting no., one step after ending no.)
+  o1 = o
+  o = 7*o
+  print("7 x",o1,"=",o)
+print()
+
+print("Multiplication table of 8:-\n")
+for p in range(1,11): #range(starting no., one step after ending no.)
+  p1 = p
+  p = 8*p
+  print("8 x",p1,"=",p)
+print()
+
+print("Multiplication table of 9:-\n")
+for q in range(1,11): #range(starting no., one step after ending no.)
+  q1 = q
+  q = 9*q
+  print("9 x",q1,"=",q)
+print()
+
+print("Multiplication table of 10:-\n")
+for r in range(1,11): #range(starting no., one step after ending no.)
+  r1 = r
+  r = 10*r
+  print("10 x",r1,"=",r)
+print()
