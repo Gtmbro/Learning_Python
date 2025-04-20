cf6ee29fa33e673a0e19ec1858e4652b5670b968
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-20 22:15:11 +0545

    Python Tutorial - Day #17 ( For loop ).py

--- Python Tutorial - Day #17 ( For loop ).py
+++ Python Tutorial - Day #17 ( For loop ).py
@@ -1,1 +1,27 @@
+name = "Ichchhya"
+for i in name:
+  print(i, end=" ")
+  if i=="a":
+    print("\nOh dear! nice to see you!")
+print()
 
+# colors = ["Red", "Green", "Blue", "Orange", "Pink"]
+# for color in colors:
+#   print(color)
+#   for j in color:
+#     print(j)
+
+print("Multiplication table of 1:-\n")
+for k in range(1,11): #range(starting no., one step after ending no.)
+  k = 1*k
+  print("1 x",k,"=",k)
+print()
+
+for m in range(5):
+  print(m)
+print()
+for n in range(1,5):
+  print(n+1)
+print()  
+for p in range(1,10,2): # range(starting no., 1 no. after ending no., step/jump)
+  print(p)
