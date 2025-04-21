a9e1ddd94bb43e3d75bf76414f8928bd19bcc784
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-21 22:06:11 +0545

    Update Python Tutorial - Day #18 ( While loops ).py

--- Python Tutorial - Day #18 ( While loops ).py
+++ Python Tutorial - Day #18 ( While loops ).py
@@ -1,1 +1,19 @@
+# print("We are going to give the square of the numbers that you enter:")
+# print("But the limit is 1000")
+# print()
 
+# i = int(input("Enter the number: "))
+# while(i<=1000):
+#   print("Square of",i,"is: ",i*i)
+#   i = int(input("Enter another number: "))
+# # print("Limit exceeded..")
+# else:
+#   print("Limit exceeded..")
+
+c = int(input("Enter any num & you'll find the nums that are smaller than that:"))
+c = c-1
+while True:
+  print(c)
+  c = c-1
+  if c<=0:
+    break
