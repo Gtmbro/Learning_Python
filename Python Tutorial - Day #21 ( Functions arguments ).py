0ddfb2b072c5903ce02bdccf2369e76a376c1453
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-25 21:45:45 +0545

    Update Python Tutorial - Day #21 ( Functions arguments ).py

--- Python Tutorial - Day #21 ( Functions arguments ).py
+++ Python Tutorial - Day #21 ( Functions arguments ).py
@@ -1,1 +1,28 @@
+def names(fname,mname,lname):
+  print("Hello",fname,mname,lname+".")
 
+def age(a,b=1):
+  print("Oh! so you are",a,"years old huh?")
+  print("In",b,"years, you would be",a+b,"years old.")
+  
+fname,mname,lname = input("Enter your name: ").split()
+a = int(input("Enter your age: "))
+
+names(fname,mname,lname)
+age(a)
+
+def average(*numbers):
+  sum = 0
+  for i in numbers:
+    sum = sum + i
+  return (sum/len(numbers))
+  
+c = average(1,2,3)
+print(c)
+
+def names(**name):
+  print("Hello",name["fname"],name["mname"],name["lname"]+".")
+
+fname,mname,lname = input("Enter your name: ").split()
+
+names(fname=fname,mname=mname,lname=lname)
