8a1aa2137688ed45f7d16714c17b5ccf3d915003
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-26 21:15:59 +0545

    Update Python Tutorial - Day #22 ( Lists ).py

--- Python Tutorial - Day #22 ( Lists ).py
+++ Python Tutorial - Day #22 ( Lists ).py
@@ -1,1 +1,23 @@
+marks = [90,86,59,69,"Ichchhya","Jodi","Amrit",6,7,9]
+# print(marks)
+# print(marks[0])
+# print(marks[-2])
 
+# if 69 in marks:
+#   print("Ye bro..")
+# else:
+#   print("Hell nah..")
+
+# if "Ich" in "Ichchhya":
+#   print("Yoo gurl..")
+# else:
+#   print("No worries..")
+
+# print(marks[4:9:2]) #marks[starting index:1 step after ending index:jump to x(2) index]
+
+lst = [i for i in range(10)]
+print(lst)
+lst1 = [i for i in range(10) if i%2==0]
+print(lst1)
+lst2 = [i*i for i in range(10) if i%2==0]
+print(lst2)
