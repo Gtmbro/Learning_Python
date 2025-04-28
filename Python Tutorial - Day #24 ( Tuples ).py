cebaadb7de08762ae358aa6678224793ebab1d19
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-28 21:03:35 +0545

    Update Python Tutorial - Day #24 ( Tuples ).py

--- Python Tutorial - Day #24 ( Tuples ).py
+++ Python Tutorial - Day #24 ( Tuples ).py
@@ -1,1 +1,11 @@
+# a = [] This is list, and it is changable
+a = (1,"Ichchhya",2,"Amrit",3,"idk") #This is tuple and it's not changable
+print(a)
+print(a[1])
+print(a[-3])
 
+if 'Ichchhya' in a:
+  print("Yes, Ichchhya is here.")
+
+b = a[1:6:2]
+print(b)
