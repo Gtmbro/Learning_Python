4e56f700daeba99fbaa1d98fa5f80d6eccb33c69
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-04-30 22:03:55 +0545

    Update Python Tutorial - Day #25 ( Operation on Tuples ).py

--- Python Tutorial - Day #25 ( Operation on Tuples ).py
+++ Python Tutorial - Day #25 ( Operation on Tuples ).py
@@ -1,1 +1,20 @@
+# names = ["Amrit","Ichchhya","Prashanna","Bishesh","Rajiv"] #Tuple
+# temp = list(names) #Tuple converted to list
+# temp.append("Amit") #Add
+# temp.pop(4) #Remove (only inde]
+# temp[0] = "Ichchhya" #Update
+# temp[1] = "Amrit "#Update
+# names = tuple(temp)
+# print(names)
+
+# countries = ("Nepal","Pakistan")
+# countries1 = ("India","Bhutan")
+# asia = countries + countries1
+# print(asia)
+
+tuple1 = (0,2,3,4,1,2,6,8,2,4,1)
+# # print(tuple1.count(2))
+# # print(tuple1.index(2))
+# print(tuple1.index(2, 5, 9)) # index(2,5,9) means search 2 from index 5 to 9, neglecting 2 if it occurs before index 5.
+print(len(tuple1))
 
