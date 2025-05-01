181364e501b8cd246237d29929e978310fffbfd6
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-05-01 21:42:41 +0545

    Update Python Tutorial - Day #26 ( Ex 2 soln, Good morning ).py

--- Python Tutorial - Day #26 ( Ex 2 soln, Good morning ).py
+++ Python Tutorial - Day #26 ( Ex 2 soln, Good morning ).py
@@ -2,3 +2,18 @@ import time
 
 yup = time.strftime('%H:%M:%S')
 print(yup)
+# hour = time.strftime('%H')
+hour = int(input("Enter your time in hour: \n"))
+print(hour)
+
+if 5<=hour<12:
+  print("Good Morning Sir")
+
+elif 12<=hour<17:
+  print("Good Afternoon Sir")
+
+elif 17<=hour<21:
+  print("Good Evening Sir")
+
+else:
+  print("Good Night Sir")
