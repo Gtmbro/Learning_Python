aef46345f2ef7ce44232d72889b2a879a4f53b17
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-05-04 21:03:05 +0545

    Update Python Tutorial - Day #28 ( f-strings ).py

--- Python Tutorial - Day #28 ( f-strings ).py
+++ Python Tutorial - Day #28 ( f-strings ).py
@@ -1,1 +1,9 @@
+boy = "Amrit"
+girl = "Ichchhya"
+print(f"{girl} is the future wife of {boy}.")
+print(f"Hey my name is {{boy}}") #Prints the curly bracket too
 
+price = 999.4999
+print(f"The price of the item is just {price:.2f} dollars!")
+
+print(f"{16+17}")
