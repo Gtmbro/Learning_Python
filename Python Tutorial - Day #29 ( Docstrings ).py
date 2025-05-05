4b8e4358301d960a0f69a3cc70d6a864ddfc1f56
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-05-05 16:22:08 +0545

    Update Python Tutorial - Day #29 ( Docstrings ).py

--- Python Tutorial - Day #29 ( Docstrings ).py
+++ Python Tutorial - Day #29 ( Docstrings ).py
@@ -1,1 +1,10 @@
+def square(a):
+  '''To find the square of the given number''' #docstring..should be above function body..
+  print(a**2)
 
+square(5)
+print(square.__doc__)
+
+# PEP 8 - Python Enhancement Proposal 8:- 
+# The ZEN of PYTHON
+import this
