0d81c887faea3bd590dc7775a3fefc9c6b9d21ec
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-05-13 16:46:49 +0545

    Update Python Tutorial - Day #33 ( Dictionaries ).py

--- Python Tutorial - Day #33 ( Dictionaries ).py
+++ Python Tutorial - Day #33 ( Dictionaries ).py
@@ -1,1 +1,20 @@
+info = {
+  "Name":"Gtm Bro",
+  "Age":"18",
+  "Hobbies":"Coding,Gym",
+  567:"Random_key"
+}
 
+# print(info[567]) 
+# print(info["Yoo"]) #Throws an error
+# print(info.get("Yoo")) #Returns none
+
+# print(info.keys())
+# print(info.values())
+
+# for i in info.keys():
+#   print(f"The corresponding value for {i} is {info[i]}")
+
+for j,k in info.items():
+  print(f"The corresponding value for {j} is {k}")
+  
