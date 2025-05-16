a183bacb427fc7fbb840ab58a31ab4ddacf86490
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-05-16 10:05:48 +0545

    Update Python Tutorial - Day #34 ( Dictionary Methods ).py

--- Python Tutorial - Day #34 ( Dictionary Methods ).py
+++ Python Tutorial - Day #34 ( Dictionary Methods ).py
@@ -1,1 +1,19 @@
+# a = {111,222,333} # Set
+# b = {122:222,222:333}, b ={} #Dictionary
+
+emp1 = {1:60, 2:49, 3:69, 4:89}
+emp2 = {5:68, 6:59, 7:69}
+
+# emp1.update(emp2) #Updates value of emp1 by adding values of emp2
+# emp1.pop(2) #Removes the key value pair of 2
+# emp1.popitem() # Removes the last key value pair
+# # emp1.clear()
+
+# a = {}
+# print({})
+
+# del emp1 # Do i have to say anything??
+del emp1[1] # Removes the key value pair of 1
+
+print(emp1)
 
