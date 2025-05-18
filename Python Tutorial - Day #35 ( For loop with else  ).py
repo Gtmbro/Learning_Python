fd66c239facbe53c7f9c484a000e7a67e0bcd4ae
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-05-18 13:25:40 +0545

    Update Python Tutorial - Day #35 ( For loop with else ).py

--- Python Tutorial - Day #35 ( For loop with else  ).py
+++ Python Tutorial - Day #35 ( For loop with else  ).py
@@ -1,1 +1,24 @@
+# for i in range(5):
+#   print(i)
+#   if i == 3:
+#     break
+# else:
+#   print("Loop has been ended successfully")
 
+# i = 0
+# while i < 5:
+#   print(i)
+#   if i == 3:
+#     break
+#   i = i + 1
+# else:
+#   print("Loop has ended")
+
+for k in range(5):
+  print("{} is printed in this step.".format(k+1))
+  if k+1 == 4:
+    print("Loop has been broken here..")
+    break
+  
+else:
+  print("All the items of loop are printed successfully.")
