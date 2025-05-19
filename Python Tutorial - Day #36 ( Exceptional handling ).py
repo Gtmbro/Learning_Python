8356b30b49b0534089d06b5f33ee110678eb2f31
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-05-19 21:12:04 +0545

    Update Python Tutorial - Day #36 ( Exceptional handling ).py

--- Python Tutorial - Day #36 ( Exceptional handling ).py
+++ Python Tutorial - Day #36 ( Exceptional handling ).py
@@ -1,1 +1,18 @@
+# a = input("Man.. you gotta enter a number now:\n")
+# print(f"Multiplication table of {a} is:\n")
+
+# try:
+#   for i in range(1,11):
+#     print(f"{a} x {i} = {int(a)*i}")
+# except:
+#   print(f"How tf do you think '{a}' is a number ??")
+
+b = [1,2,3]
+try:
+  num = int(input("Enter an integer: "))
+  print(b[num])
+except ValueError:
+  print("It's a value error.")
+except IndexError:
+  print("It's an index error.")
 
