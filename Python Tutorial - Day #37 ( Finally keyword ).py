ca458c350789d625a51da16e50c33307b52726de
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-05-20 15:42:24 +0545

    Update Python Tutorial - Day #37 ( Finally keyword ).py

--- Python Tutorial - Day #37 ( Finally keyword ).py
+++ Python Tutorial - Day #37 ( Finally keyword ).py
@@ -1,1 +1,15 @@
-
+def yoo():
+  try:
+    a = [2,4,6,8]
+    i = int(input("Enter the index: "))
+    return (f"{a[i]} is what you needed?")
+     
+  except:
+    return("Some error occured!")
+     
+  finally:
+    print("I am always executed..")
+  # print("yooo")
+  
+x = yoo()
+print(x)
