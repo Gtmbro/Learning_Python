7b7bc2061e10ef04d9d6f24b9d697297bc9d9f3b
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-05-21 22:20:46 +0545

    Update Python Tutorial - Day #38 ( Raising custom error ).py

--- Python Tutorial - Day #38 ( Raising custom error ).py
+++ Python Tutorial - Day #38 ( Raising custom error ).py
@@ -1,1 +1,15 @@
+class mfError(Exception):
+  pass
+  
+input = input("Enter value between 6 to 10: ")
+  
+if input.lower() == "quit":
+  raise mfError("Mf haha")
+  
+try:
+  a=int(input)
+  if a<6 or a>10:
+    raise ValueError("Value should be between 6 and 10")
 
+except ValueError as i:
+  print(f"{i} caused some error")
