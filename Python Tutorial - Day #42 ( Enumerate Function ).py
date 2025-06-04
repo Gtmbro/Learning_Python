f6d88db3accae7a8f187391ddb2c22243ff22f47
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-04 17:43:33 +0545

    Update Python Tutorial - Day #42 ( Enumerate Function ).py

--- Python Tutorial - Day #42 ( Enumerate Function ).py
+++ Python Tutorial - Day #42 ( Enumerate Function ).py
@@ -1,1 +1,9 @@
+sp = "\n"
 
+names = ["Jake","Jenna","Ichchhya","Tyler","Madison"]
+
+for a,i in enumerate(names, start=1):
+  # print(f"{a} {i}")
+  print(f"{a} {i}") if i!= "Ichchhya" else print(f"{sp}I found you, {i} {sp}")
+  # if i == "Ichchhya":
+  #   print(f"{sp}I found you, {i} {sp}")
