2e9b71bf639502270133d436d99972d1e0b48f6f
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-09 17:00:07 +0545

    Update Python Tutorial - Day #44 ( How import works in python).py

--- Python Tutorial - Day #44 ( How import works in python).py
+++ Python Tutorial - Day #44 ( How import works in python).py
@@ -1,1 +1,24 @@
+from amrit import amrit, greeting
+
+greeting()
+print(amrit)
+
+
+
+# import math
+# print(math.sqrt(16))
+
+# import math as m
+# from math import sqrt, pi, factorial, pow
+# from math import * #imports all functions from math module
+# from math import sqrt as s, pi, factorial as f, pow as p
+# from math import pi, pow as p, factorial as f, sqrt as s
+
+# print(s(16))
+# print(pi*p(2,4)) # (x,y) = x raised to the power y
+# print(f(6))
+
+# import math
+# print(dir(math)) # prints all the names in math module, including functions, variables, etc.
+# print(math.inf, type(math.inf))
 
