9c4615c78ff62b1542951db91211ac57bb850753
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-12 22:27:00 +0545

    Update Python Tutorial - Day #46 ( OS Module in python ).py

--- Python Tutorial - Day #46 ( OS Module in python ).py
+++ Python Tutorial - Day #46 ( OS Module in python ).py
@@ -1,1 +1,28 @@
+#main.py:-
 
+import os
+
+if (not os.path.exists("tutorial")):
+    os.mkdir("tutorial")
+
+for i in range(1,11):
+    os.mkdir(f"tutorial/Day{i}")
+
+#rename.py:-
+
+import os
+
+for i in range(1,11):
+    os.rename(f"tutorial/Day{i}",f"tutorial/ Night{i}")
+
+#findlists.py:-
+
+import os
+
+folders = os.listdir("tutorial")
+
+print(folders)
+
+for i in folders:
+    print(i)
+    print(os.listdir(f"tutorial/{i}"))
