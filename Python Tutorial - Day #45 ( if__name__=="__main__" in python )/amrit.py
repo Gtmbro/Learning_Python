6c72137011280a8a675931dd789fb22b95e288a2
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-13 16:12:01 +0000

    Added amrit.py to Day 45

---
+++ Python Tutorial - Day #45 ( if__name__=="__main__" in python )/amrit.py
@@ -0,0 +1,5 @@
+def greeting():
+  print("Hey! Good morning!")
+
+if __name__=="__main__": #This means if it's running from this file, then execute the code below and if is being imported, dont execute it.
+  greeting()
