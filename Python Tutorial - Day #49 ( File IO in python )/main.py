7a4922b3c762def227e718c3024dcd89e4b8ecb1
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-18 21:24:33 +0545

    Update main.py

--- Python Tutorial - Day #49 ( File IO in python )/main.py
+++ Python Tutorial - Day #49 ( File IO in python )/main.py
@@ -1,1 +1,9 @@
+text = [
+  "This file is for explaining what is happening in the code.\n",
+  "This will be explained as the code becomes more complex."
+]
+
+with open('explain.txt','w') as a:
+  # a.write(text) If the variable doen't have any list
+  a.writelines(text) # If the variable has a list
 
