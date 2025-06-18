866c2094d9e9781e9445de15e5e04dac81beb11c
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-18 16:03:07 +0000

    Read and appended items in explain.txt

--- Python Tutorial - Day #49 ( File IO in python )/main.py
+++ Python Tutorial - Day #49 ( File IO in python )/main.py
@@ -1,9 +1,16 @@
-text = [
-  "This file is for explaining what is happening in the code.\n",
-  "This will be explained as the code becomes more complex."
-]
+# text = [
+#   "This file is for explaining what is happening in the code.\n",
+#   "This will be explained as the code becomes more complex."
+# ]
 
-with open('explain.txt','w') as a:
-  # a.write(text) If the variable doesn't have any list.
-  a.writelines(text) # If the variable has a list
+# with open('explain.txt','w') as a:
+#   # a.write(text) If the variable doesn't have any list.
+#   a.writelines(text) # If the variable has a list
 
+with open('explain.txt','r') as b:
+  item = b.read()
+  print(item)
+
+with open('explain.txt','a') as c:
+  appended = c.write("\nThis is the appended text.")
+  print(appended)
