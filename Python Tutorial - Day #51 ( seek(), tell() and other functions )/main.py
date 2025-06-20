a7ece5e08ff133e8df96af0fba67d76e9f1d9e58
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-20 15:11:01 +0545

    Update main.py

--- Python Tutorial - Day #51 ( seek(), tell() and other functions )/main.py
+++ Python Tutorial - Day #51 ( seek(), tell() and other functions )/main.py
@@ -1,1 +1,30 @@
+code = """#Unnecessary line 1
+# Unnecessary line 2
+# Here's the code:-
+def avg():
+  a = int(input("Enter"))
+  b = int(input("Enter"))
+  print((a+b)/2)
 
+avg()
+"""
+
+with open('new.py','w') as d:
+  d.write("#Hello guys.")
+  d.truncate(6) #Only shows specified characters.
+  
+with open('new.py','a') as f:
+  f.write("\n\n")
+  f.write(code)
+
+with open('new.py','r') as a:
+  f = a.readline() #Reads 1st line.
+  s = a.readline() #Reads 2nd line.
+  t = a.readline() #Reads 3rd line.
+  fo = a.readline() #Reads 4th line.
+  skip = len(f) + len(s) + len(t) + len(fo)
+  a.seek(skip) #Skips the required bytes/characters.
+  b = a.read() #Reads specific bytes if specified, if not, reads the remaining content.
+  print(b)
+
+  print(a.tell()) #Tells how many bytes are skipped.
