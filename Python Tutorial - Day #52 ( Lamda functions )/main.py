25d46575b224d77f0ffaf28df104bb9400b14737
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-22 21:21:50 +0545

    Update main.py

--- Python Tutorial - Day #52 ( Lamda functions )/main.py
+++ Python Tutorial - Day #52 ( Lamda functions )/main.py
@@ -1,1 +1,20 @@
 
+# def fibonacci(n):
+#   a = 0
+#   b = 1
+#   for _ in range(n):
+#     print(a,end=" ")
+#     a , b = b , a+b #Python evaluates right side and then assigns values to left side.
+
+# fibonacci(4)
+
+def akl(fx, value):
+  return 2 + fx(value)
+
+# avg = lambda a,b : (a+b)/2
+# cube = lambda y: y*y*y
+
+# print(cube(3))
+# print(avg(4,5))
+
+print(akl(lambda x: x*x ,2))
