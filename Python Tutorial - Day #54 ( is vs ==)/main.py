a1996d9227e8b3ebb7a6d4f9a0910360feb833ba
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-25 20:05:36 +0545

    Update main.py

--- Python Tutorial - Day #54 ( is vs ==)/main.py
+++ Python Tutorial - Day #54 ( is vs ==)/main.py
@@ -1,1 +1,18 @@
+# x = [1, 2, 3]
+# y = [1, 2, 3]
+# print(x is y)   # False, even though values are same
+# print(x == y)   # True, because values are same
 
+def get_data_from_user():
+  response = input("Enter something or leave blank: ").strip()
+  if response == "":
+      return None
+  return response
+
+data = get_data_from_user()
+
+# if data == "None": checks for the literal string "None"
+if data is None:
+  print("User gave no input")
+else:
+  print(data)
