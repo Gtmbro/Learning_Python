46ef0f01ea8a65454f16331a6cbcc3bc345002d2
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-26 21:15:36 +0545

    Update main.py

--- Python Tutorial - Day #55 ( Ex 5: Snake Water Gun )/main.py
+++ Python Tutorial - Day #55 ( Ex 5: Snake Water Gun )/main.py
@@ -1,1 +1,22 @@
 
+import random
+
+options = ["Snake","Water","Gun","Snake","Water","Gun","Snake","Water","Gun"]
+
+superior = {"Water":"Gun",
+         "Snake":"Water",
+         "Gun":"Snake"}
+
+def game():
+  if user == comp:
+    return "Draw!"
+  elif superior[user] == comp:
+    return "You Win!"
+  else:
+    return "You Lose!"
+
+comp = random.choice(options)
+user = input("Enter your choice:\nSnake\nWater\nGun\n\n").strip().capitalize()
+
+final = game()
+print(final)
