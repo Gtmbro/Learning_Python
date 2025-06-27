5af5b588b8f3310ec03ac06b9dab694d9217ebe2
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-27 21:29:14 +0545

    Update main.py

--- Python Tutorial - Day #56 ( Introduction To OOPS )/main.py
+++ Python Tutorial - Day #56 ( Introduction To OOPS )/main.py
@@ -1,1 +1,6 @@
+class detail(): #User defined class/template for the object
+  name = ""
+  age = ""
 
+a = detail() #a now has template of detail()
+a.name = "xyz" #a stores the info in name variable in the template
