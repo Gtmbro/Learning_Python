d92ff16e54637e9a3ec10a96a48d390d703d847c
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-27 21:47:47 +0545

    Update main.py

--- Python Tutorial - Day #57 ( Classes and objects )/main.py
+++ Python Tutorial - Day #57 ( Classes and objects )/main.py
@@ -1,1 +1,17 @@
 
+class add_exercise:
+  name= ""
+  sets = 0
+  reps = 0
+  weight = 0
+  def volume(self):
+    return f"Your total volume for {self.name} is {self.sets*self.reps*self.weight}kg."
+
+a = add_exercise()
+
+a.name = input("Enter the exercise: ").capitalize()
+a.sets = int(input("Enter the sets: "))
+a.reps = int(input("Enter the reps: "))
+a.weight = int(input("Enter the weight in kg: "))
+
+print(a.volume())
