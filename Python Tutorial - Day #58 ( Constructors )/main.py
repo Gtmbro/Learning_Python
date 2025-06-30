8c80da6698c59ce0decdaf59e2289aba7be60298
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-06-30 21:00:20 +0545

    Update main.py

--- Python Tutorial - Day #58 ( Constructors )/main.py
+++ Python Tutorial - Day #58 ( Constructors )/main.py
@@ -1,1 +1,13 @@
 
+class person:
+  def __init__(self, a, b): #__init__():- constructor
+    self.name = a
+    self.occupation = b
+
+  def info(self):
+    print(f"{self.name} is a {self.occupation}")
+
+a = person("Ana","Nan")
+b = person("Nan","Ana")
+a.info()
+b.info()
