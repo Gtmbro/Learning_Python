a869c650f7b1ace085bed46c50bbe26344ce2956
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-07-02 21:13:52 +0545

    Update main.py

--- Python Tutorial - Day #59 ( Decorators )/main.py
+++ Python Tutorial - Day #59 ( Decorators )/main.py
@@ -1,1 +1,21 @@
+def greet(f): 
+  def greeting(*args,**kwargs):
+    print("Good Morning")
+    f(*args,**kwargs)
+    print("Hey buddy")
+  return greeting # returns the result of greeting to greet.
+
+
+@greet #sends the function factorial to greet to run
+def factorial(n):
+  a = 1
+  for i in range(1,n):
+    a *= (n-i)
+  print(n*a)
+
+def sum(a,b):
+  print(a+b)
+
+sum(1,2)
+factorial(5)
 
