dd8b56f94f7856c1d21dd984889e744327b791d1
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-07-08 21:09:52 +0545

    Update main.py

--- Python Tutorial - Day #63 ( Ex soln: Snake Water Gun )/main.py
+++ Python Tutorial - Day #63 ( Ex soln: Snake Water Gun )/main.py
@@ -1,1 +1,40 @@
 
+import random
+
+choices = ["Snake","Water","Gun"]
+comp = random.choice(choices) #choice for a single item
+
+user = int(input("Enter 1 for Snake\nEnter 2 for water\nEnter 3 for gun: "))
+
+try:
+  if user == 1:
+    user_choice = "Snake"
+  elif user == 2:
+    user_choice = "Water"
+  elif user == 3:
+    user_choice = "Gun"
+  else:
+    print("Please enter from options!")
+    exit()
+
+except ValueError:
+  print("Invalid input.!")
+  exit()
+  
+superior = {
+  "Snake":"Water",
+  "Water":"Gun",
+  "Gun":"Snake"
+}
+
+def game(user_choice, comp_choice):
+  if user_choice == comp_choice:
+    return"Draw!"
+  elif superior[user_choice] == comp_choice:
+    return"You Won!"
+  else:
+    return "You Lost!"
+
+print(f"\nYou chose: {user_choice}")
+print(f"Computer chose: {comp}")
+print(game(user_choice, comp))
