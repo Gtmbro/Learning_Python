b20f723074047fae15884abbc393fe944ba518a9
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-07-13 21:25:03 +0545

    Update main.py

--- Python Tutorial - Day #66 ( Instance vs class variables )/main.py
+++ Python Tutorial - Day #66 ( Instance vs class variables )/main.py
@@ -1,1 +1,20 @@
 
+class Student:
+  SchoolName = "Horizon" #class variable
+  
+  def __init__(self, name):
+    self.marks = 80 #instance variable
+    self.nm = name #instance variable
+
+  def ShowDetails(self):
+    print(f"The name of the student is: {self.nm} and school name is: {self.SchoolName}")
+    print(f"His marks is {self.marks}")
+   
+std1 = Student("Amrit")
+std1.marks = 90 #instance variable
+std1.SchoolName = "Stanford" #instance variable
+Student.SchoolName = "Hardward" #Class variable, it's changed for all instances
+std1.ShowDetails()
+# Student.ShowDetails(std1)
+std2 = Student("John")
+std2.ShowDetails()
