6b4f6f2b69f0e3dcd86673d1f843cb6e0b10cf78
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-07-21 15:37:03 +0545

    Update main.py

--- Python Tutorial - Day #67 ( Ex 6 soln: Library Management System )/main.py
+++ Python Tutorial - Day #67 ( Ex 6 soln: Library Management System )/main.py
@@ -1,4 +1,3 @@
-
 book_objs_list = []
 
 class Library:
@@ -14,9 +13,6 @@ class Library:
     
   def show_author(self):
     return self.author
-
-  def show_no_of_books(self):
-    return self.no_of_books
 
   def length_book(self):
     return len(self.book)
@@ -35,18 +31,21 @@ while decision == "y":
   book_name = input("\nEnter the name of the book: ").capitalize()
   author = input(f"\nEnter the name of the author of {book_name}: ").capitalize()
 
+
+  if not book_name and not author:
+    break
   book_obj = Library(book_name, author)
   adder(book_obj)
-
+  
   decision = input("\nEnter 'y' to add information again\n"
                    "Anything else to exit: ").lower()
 
+if not book_objs_list:
+  exit()
 #-------------------------EXECUTION------------------------------
 number = Library.no_of_books
 
-if not book_objs_list:
-  exit()
-elif number == 1:
+if number == 1:
   print("\nYou have added 1 book in total.\n")
 elif number != 1:
   print(f"\nYou have added {number} books in total.\n")
@@ -56,7 +55,7 @@ max_l_book = max(a.length_book() for a in book_objs_list)
 max_l_author = max(a.length_author() for a in book_objs_list)
 
 print(f"| {'Book':<{max_l_book}} | {'Author':<{max_l_author}} |")
-print("-"*(max_l_book + max_l_author + 9))
+print("-"*(max_l_book + max_l_author + 7))
 
 #--------------------------Result---------------------------------
 for book in book_objs_list:
