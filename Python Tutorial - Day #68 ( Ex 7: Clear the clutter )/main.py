99b693dd137137f777329706abde1290c3d9f62c
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-07-25 20:20:11 +0545

    Update main.py

--- Python Tutorial - Day #68 ( Ex 7: Clear the clutter )/main.py
+++ Python Tutorial - Day #68 ( Ex 7: Clear the clutter )/main.py
@@ -1,10 +1,13 @@
-
 import os
 
-i = 1
-for file in os.listdir():
-  if file.endswith(".png"):
-    new_name = f"{i}.png"
-    if file != new_name:
-      os.rename(file,new_name)
-    i += 1
+def rename(format):
+  i = 1
+  for file in os.listdir():
+    if file.endswith(f".{format}") and file != "main.py":
+      new_name = f"{i}.{format}"
+      if file != new_name:
+        os.rename(file,new_name)
+        i += 1
+
+input = input("Enter your file format ( for eg: pdf, txt ): ")
+output = rename(input)
