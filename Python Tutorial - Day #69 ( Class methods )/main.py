f48213a3f4dc3b5c8cc38bf87601b7d7e2f09f25
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-07-25 20:39:31 +0545

    Update main.py

--- Python Tutorial - Day #69 ( Class methods )/main.py
+++ Python Tutorial - Day #69 ( Class methods )/main.py
@@ -1,1 +1,14 @@
+class Student:
+  School = "Horizon"
+  def info(self):
+    print(f"The school of {self.name} is {self.School}")
 
+  @classmethod
+  def change(idk, new_school):
+    idk.School = new_school
+
+s1 = Student()
+s1.name = "Amrit"
+s1.info()
+s1.change("Shanti")
+s1.info()
