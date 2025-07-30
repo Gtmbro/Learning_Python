11808114150c90528396cafd23a379abdee2b918
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-07-30 20:52:18 +0545

    Update main.py

--- Python Tutorial - Day #71 ( dir,__dict__ and help method )/main.py
+++ Python Tutorial - Day #71 ( dir,__dict__ and help method )/main.py
@@ -1,1 +1,10 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = int(age)
+    self.year = 2025
 
+person = Person("John",18)
+# print(dir(person))
+# print(help(person))
+print(person.__dict__)
