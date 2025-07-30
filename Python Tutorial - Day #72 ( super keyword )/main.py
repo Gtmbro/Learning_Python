7fb1ca9a38b34461c498a886c23d1f4a8bef06fd
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-07-30 21:17:12 +0545

    Update main.py

--- Python Tutorial - Day #72 ( super keyword )/main.py
+++ Python Tutorial - Day #72 ( super keyword )/main.py
@@ -1,1 +1,20 @@
 
+class Father:
+  def __init__(self, father_name, surname):
+    self.father_name = father_name
+    self.surname = surname
+  def info(self):
+    print(f"I am {self.father_name} {self.surname}.")
+
+class Son(Father):
+  def __init__(self, name, father_name, surname):
+    super().__init__(father_name, surname)
+    self.name = name
+  def info(self):
+    print(f"I am {self.name} {self.surname}, {self.father_name} {self.surname}'s son.")
+
+
+father = Father("George","Bush")
+son = Son("John","George","Bush")
+father.info()
+son.info()
