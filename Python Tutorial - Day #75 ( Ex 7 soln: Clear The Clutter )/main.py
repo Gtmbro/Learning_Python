d982d32b147a79ed0e14feee1b02f18704b7024e
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-05 18:36:53 +0545

    Update main.py

--- Python Tutorial - Day #75 ( Ex 7 soln: Clear The Clutter )/main.py
+++ Python Tutorial - Day #75 ( Ex 7 soln: Clear The Clutter )/main.py
@@ -3,10 +3,20 @@ import os
 def splitter(file,index):
  return os.path.splitext(file)[index]
 
-i = 1
-for file in os.listdir():
-  if splitter(file,1) == ".txt" or splitter(file,1) == ".jpeg" or splitter(file,1) == ".png":
-    new_name = f"{os.path.splitext(file)[0]}{i}.{os.path.splitext(file)[1]}"
-    if file != new_name:
-      os.rename(file,new_name)
-    i += 1
+def cleaner(format):
+  i = 1
+  for file in os.listdir():
+    if splitter(file,1) == f".{format}":
+      new_name = f"{i}.{splitter(file,1)}"
+      if file != new_name:
+        os.rename(file, new_name)
+      i += 1
+
+format = input("Enter your format (eg: png, txt, jpeg):\n")
+cleaner(format)
+
+decision = input("Enter 1 to do it again:\n")
+
+while decision == "1":
+  format = input("Enter your format (eg: png, txt, jpeg):\n")
+  cleaner(format)
