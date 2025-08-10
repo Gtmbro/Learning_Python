7c5235b383940de56a657ed809e93b7820dab15a
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-10 21:19:11 +0545

    Update main.py

--- Python Tutorial - Day #77 ( Operator Overloading )/main.py
+++ Python Tutorial - Day #77 ( Operator Overloading )/main.py
@@ -1,1 +1,18 @@
+class Vector:
+  def __init__(self, i, j, k):
+    self.i = i
+    self.j = j
+    self.k = k
 
+  #__add__ method overloaded '+' operator.
+  def __add__(self, other):
+    return Vector(self.i + other.i, self.j + other.j, self.k + other.k)
+
+  def __str__(self):
+    return f"{self.i}i + {self.j}j + {self.k}k"
+
+v1 = Vector(1,3,5)
+v2 = Vector(2,4,8)
+
+print(v1+v2)
+print(type(v1)) #Type of v1 or v2
