6c84d07933ec4f752c268bcffbb94b33b5d14e71
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-10 20:49:09 +0545

    Update main.py

--- Python Tutorial - Day #76 ( Ex 8: Merge the pdf )/main.py
+++ Python Tutorial - Day #76 ( Ex 8: Merge the pdf )/main.py
@@ -1,1 +1,15 @@
+import os
 
+from pypdf import PdfReader #To import class PdfReader.
+from pypdf import PdfWriter #To import PdfWriter class.
+
+writer = PdfWriter() # To create instance of the PdfWriter class.
+
+for file in os.listdir():
+  if file.endswith("pdf") and os.path.getsize(file) != 0:
+    reader = PdfReader(file)
+    for page in reader.pages:
+      writer.add_page(page)
+
+with open("Merged_Pdf.pdf",'wb') as f:
+  writer.write(f)
