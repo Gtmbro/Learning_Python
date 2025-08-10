a69e58bc2410dd99764bc131f405f603e9848796
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-10 21:34:55 +0545

    Update main.py

--- Python Tutorial - Day #78 ( Single Inheritance )/main.py
+++ Python Tutorial - Day #78 ( Single Inheritance )/main.py
@@ -1,1 +1,18 @@
+class Animal:
+  def __init__(self, species, sound):
+    self.species = species
+    self.sound = sound
 
+  def make_sound(self):
+    print(f"{self.species} makes {self.sound}")
+
+class Cat(Animal):
+  def __init__(self, name, species="Cat", sound="Meow"):
+    Animal.__init__(self, species, sound)
+    self.name = name
+
+  def make_sound(self):
+    print(f"{self.name} is a Cat of species '{self.species}' and it makes '{self.sound}' sound")
+
+a = Cat("Bella")
+a.make_sound()
