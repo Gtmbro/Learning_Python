509c694f6c897c62fa67921e0d4c0bdd19dafe1e
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-10 21:53:25 +0545

    Update main.py

--- Python Tutorial - Day #79 ( Multiple Inheritance )/main.py
+++ Python Tutorial - Day #79 ( Multiple Inheritance )/main.py
@@ -1,1 +1,25 @@
+class Father:
+  def __init__(self, father):
+    self.father = father
 
+  def show(self):
+    return(f"Father is: {self.father}")
+
+class Mother:
+  def __init__(self, mother):
+    self.mother = mother
+
+  def show(self):
+    return(f"Mother is: {self.mother}")
+
+class Family(Father, Mother): #Class family inherited Father and Mother classes.
+  def __init__(self, father, mother, son):
+    self.father = father
+    self.mother = mother
+    self.son = son
+
+  def show(self):
+    print(f"{Father.show(self)}\n{Mother.show(self)}\nAnd son is: {self.son}")
+
+home = Family("John","Lia","Alex")
+home.show()
