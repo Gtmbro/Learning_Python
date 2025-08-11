fdd6bee44cf122e8853c309aba24df09048fb64d
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-11 18:49:28 +0545

    Update main.py

--- Python Tutorial - Day #82 ( Ex 8 solution: Merge the pdf )/main.py
+++ Python Tutorial - Day #82 ( Ex 8 solution: Merge the pdf )/main.py
@@ -1,1 +1,11 @@
+import os
+from pypdf import PdfWriter
 
+merger = PdfWriter() #Assigning class PdfWriter to Writer
+
+for file in os.listdir():
+  if file.endswith("pdf"):
+    merger.append(file) #Giving variable file to append method of PdfWriter
+
+merger.write("Merged_Pdf.pdf")
+merger.close()
