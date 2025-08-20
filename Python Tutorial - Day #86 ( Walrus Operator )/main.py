4dc55b6dd75cee5f88a5ae9f7b5441f9f72fdab0
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-20 20:36:15 +0545

    Update main.py

--- Python Tutorial - Day #86 ( Walrus Operator )/main.py
+++ Python Tutorial - Day #86 ( Walrus Operator )/main.py
@@ -1,1 +1,8 @@
+print("."*55 + "Language Logger" + "."*55)
 
+languages = list()
+print("Enter 'quit' to quit.")
+while (a := input("Enter the language: ")) != "quit": # := walrus operator,which is an expression that can be used in the same line and returns a value.
+  languages.append(a)
+
+print(f"Favourite languages: {languages}")
