81fc850643948fa7fdf7453e8cc57a23f34bda6e
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-20 21:49:34 +0545

    Update main.py

--- Python Tutorial - Day #87 ( Shutil Module )/main.py
+++ Python Tutorial - Day #87 ( Shutil Module )/main.py
@@ -1,1 +1,9 @@
+import os
+import shutil
 
+# shutil.copy("main.py","copied.py")
+# os.rename("copied.py","copied-file.py")
+# shutil.move("copied-file.py","Runner/copied_file.py")
+# shutil.copytree("Runner","Copied_Folder")
+
+shutil.rmtree("Copied_Folder")
