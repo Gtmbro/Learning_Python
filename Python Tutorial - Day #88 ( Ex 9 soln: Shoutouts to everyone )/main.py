dd23a096b8b698627606735f6ef1f01d6994d742
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-21 08:20:53 +0545

    Update main.py

--- Python Tutorial - Day #88 ( Ex 9 soln: Shoutouts to everyone )/main.py
+++ Python Tutorial - Day #88 ( Ex 9 soln: Shoutouts to everyone )/main.py
@@ -1,3 +1,14 @@
+# For Mac
+
+# from os import system
+
+# names = ["Sundar Pichai","Bill Gates","Steve Jobs","Mark Zukerburg","Tim Cook"]
+
+# for name in names:
+#   system(f"Shoutout to {name}")
+
+#For Windows:-
+
 from win32com.client import Dispatch
 
 speaker = Dispatch("SAPI.SpVoice")
