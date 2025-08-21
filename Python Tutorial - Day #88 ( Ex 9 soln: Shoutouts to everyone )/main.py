3a1eacf2467722d661b63bbdd385726186796a03
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-21 08:17:08 +0545

    Update main.py

--- Python Tutorial - Day #88 ( Ex 9 soln: Shoutouts to everyone )/main.py
+++ Python Tutorial - Day #88 ( Ex 9 soln: Shoutouts to everyone )/main.py
@@ -1,1 +1,7 @@
+from win32com.client import Dispatch
 
+speaker = Dispatch("SAPI.SpVoice")
+
+names = ["Sundar Pichai","Bill Gates","Steve Jobs","Mark Zukerburg","Tim Cook"]
+for name in names:
+    speaker.speak(f"Shoutout to {name}")
