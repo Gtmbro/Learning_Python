9e50bfd550db0b461b011e39d0b4730ff1768901
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-31 19:47:31 +0545

    Update main.py

--- Python Tutorial - Day #90 ( Ex 10: News App )/main.py
+++ Python Tutorial - Day #90 ( Ex 10: News App )/main.py
@@ -1,1 +1,27 @@
+import requests
 
+API = "" #<- Put your api key from newsapi here.
+url = "https://newsapi.org/v2/top-headlines"
+
+params = {
+    "country":"us",
+    "category":"Technology",
+    "pageSize": 5,
+}
+
+headers = {
+    "X-Api-key": API
+}
+
+response = requests.get(url, params=params, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    articles = data["articles"]
+
+    for i, article in enumerate(articles):
+        print(f"{i}. {article['title']}")
+        print(f" Source: {article['source']['name']}")
+        print(f" URL: {article['url']}\n")
+else:
+    print("Error: ",response.status_code, response.text)
