4cea5116b43802afee4e187601d9599b9ba5437d
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-08-31 20:28:49 +0545

    Update main.py

--- Python Tutorial - Day #91 ( Generators )/main.py
+++ Python Tutorial - Day #91 ( Generators )/main.py
@@ -1,1 +1,20 @@
+import random
 
+def number_war():
+  print("\nLet's compare some numbers..")
+  comp = random.randint(1,100)
+  while True:
+    user = yield f"\nComp has chosen: {comp}"
+    if user is None:
+      continue
+    yield f"\nComp: {comp}\nYou: {user}\nHigher: {max(comp, user)}"
+
+    comp = random.randint(1,100)
+
+gen = number_war()
+
+print(next(gen))
+print(gen.send(10))
+
+print(next(gen))
+print(gen.send(20))
