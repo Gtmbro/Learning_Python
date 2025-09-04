0c6520b0ec1e972a5230a96363a6eca8800249f6
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-09-04 21:26:40 +0545

    Update main.py

--- Python Tutorial - Day #93 ( Ex 10 soln: News app )/main.py
+++ Python Tutorial - Day #93 ( Ex 10 soln: News app )/main.py
@@ -1,1 +1,29 @@
+import requests
+import json
 
+API_KEY = "" #newsapi key here
+
+topic = input("Give your topic: ")
+country = input("Enter your country's code ( 2 letters ): ")
+pagesize = input("Enter page size: ")
+
+url = f"https://newsapi.org/v2/top-headlines?"
+
+params = {
+    "apiKey":API_KEY,
+    "country":country,
+    "category":topic,
+    "pageSize":pagesize
+}
+
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    articles = data["articles"]
+    # print(data)
+
+    for article in articles:
+        print(f"Title: {article['title']}")
+        print(f"Description:\n {article['description']}")
+        print(f"Url: {article['url']}")
