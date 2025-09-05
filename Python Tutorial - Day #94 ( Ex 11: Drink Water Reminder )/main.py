97cb4a623828676ea549fd902fcedabbd7d61085
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-09-05 22:05:41 +0545

    Update main.py

--- Python Tutorial - Day #94 ( Ex 11: Drink Water Reminder )/main.py
+++ Python Tutorial - Day #94 ( Ex 11: Drink Water Reminder )/main.py
@@ -1,1 +1,22 @@
 
+import os
+import shutil
+from plyer import notification
+import time 
+
+while True:
+    for item in os.listdir():
+        if item.lower() == "stop.txt": #Create stop.txt to terminate the file.
+            print("Reminder has been terminated..")
+
+            if os.path.isfile(item):
+                os.remove(item)
+            elif os.path.isdir(item):
+                shutil.rmtree(file)
+            exit()
+    notification.notify(
+        title="Drink Water Reminder",
+        message="Drink Water bruh.!!!",
+        timeout=10
+    )
+    time.sleep(7200)
