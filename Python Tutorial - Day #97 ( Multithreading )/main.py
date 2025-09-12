ce2b41e2b5e2b3b60ff3342501104357fe45f099
Gtmbro <121627329+Gtmbro@users.noreply.github.com>
2025-09-12 13:44:06 +0545

    Update main.py

--- Python Tutorial - Day #97 ( Multithreading )/main.py
+++ Python Tutorial - Day #97 ( Multithreading )/main.py
@@ -1,1 +1,16 @@
+import threading
+import time
+import math
 
+def executer(func, n):
+    t = threading.Thread(target=func, args=[n]) #Creates new thread object.
+    t.start() #Starts our thread
+    print(t) #Prints the thread object
+    t.join() #to make it wait till main thread finishes.
+    
+def sqroot(num):
+    time.sleep(2)
+    print(math.sqrt(num))
+
+t1 = executer(sqroot, 4)
+t2 = executer(sqroot, 9)
